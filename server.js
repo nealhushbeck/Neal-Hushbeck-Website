97abd139819ddd0d0bc49866c2150457930a1830
nealhushbeck <nealhushbeck@outlook.com>
2026-05-04 16:03:26 -0600

    Fix static file serving path in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/gridImages', (req, res) => {
   const dirPath = path.join(__dirname, 'public/gridImages');
