3891f632e518d936a7eff3c8f2823472b1d08567
Neal Hushbeck <nealhushbecl@outlook.com>
2026-05-04 14:59:22 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/gridImages', (req, res) => {
+  const dirPath = path.join(__dirname, 'public/gridImages');
+  fs.readdir(dirPath, (err, files) => {
+    if (err) return res.status(500).send('Error loading images');
+    res.json(files);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
