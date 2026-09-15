31fcda398533dc19e6dc31d5702a425ed1caf0a4
Neal Hushbeck <nealhushbeck@outlook.com>
2026-09-14 23:55:18 -0600

    Update website

---
+++ server.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/gridImages', (req, res) => {
+  const dirPath = path.join(__dirname, 'public/gridImages');
+
+  fs.readdir(dirPath, (err, files) => {
+    if (err) return res.status(500).send('Error loading images');
+
+    res.json(files.map(file => `/gridImages/${file}`));
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
